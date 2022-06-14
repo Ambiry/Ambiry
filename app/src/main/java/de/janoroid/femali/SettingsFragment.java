eb1b4fdad6b5b00a993ece6e5cb8f3a29375eb5f
Juljano <moellerjuljano@gmail.com>
2022-06-14 20:29:15 +0200

    init

--- app/src/main/java/de/janoroid/femali/SettingsFragment.java
+++ app/src/main/java/de/janoroid/femali/SettingsFragment.java
@@ -1,8 +1,5 @@
 package de.janoroid.femali;
 import android.os.Bundle;
-
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
@@ -13,20 +10,16 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
 
-        //The Backbutton
-        ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
-
-        if (actionBar != null)
-        {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setDisplayShowHomeEnabled(true);
-        }
-
-
         setPreferencesFromResource(R.xml.preference, rootKey);
-
         Preference preference = findPreference("Notification_preference");
-        preference.setOnPreferenceClickListener(preference1 -> false);
+
+
+        preference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                return false;
+            }
+        });
 
     }
 
